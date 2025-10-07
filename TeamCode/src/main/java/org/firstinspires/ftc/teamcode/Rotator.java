86eb5f004bc06a1af281d896f4ccf4df2133e924
Zevatronn <zevborch@gmail.com>
2025-10-06 22:23:22 -0700

    added more stuf for a field relitive drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rotator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rotator.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Rotator {
-    public double rotateX(double x, double y, double rot){
-        rot *= (Math.PI/180);
-        x = x * Math.cos(rot) - y * Math.sin(rot);
+    public double rotateX(double x, double y, double rot, Button rBump){
+        if(rBump.pressing()) {
+            rot *= (Math.PI / 180);
+            x = x * Math.cos(rot) - y * Math.sin(rot);
+        }
         return x;
     }
-    public double rotateY(double x, double y, double rot){
-        rot *= (Math.PI/180);
-        x = x * Math.sin(rot) + y * Math.cos(rot);
+    public double rotateY(double x, double y, double rot, Button rBump){
+        if(rBump.pressing()) {
+            rot *= (Math.PI / 180);
+            x = x * Math.sin(rot) + y * Math.cos(rot);
+        }
         return y;
+
     }
 }
