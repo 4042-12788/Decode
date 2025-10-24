24c666d18bf22ad59898c2127c2a59565a320141
Garfield Robotics <contact@garfieldrobotics.com>
2025-10-23 17:27:34 -0700

    auto testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoActionHandler.java
@@ -0,0 +1,188 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+//import static org.firstinspires.ftc.teamcode.control.auto_execution.AutoActions.DELIVER;
+//import static org.firstinspires.ftc.teamcode.control.auto_execution.AutoActions.DROP;
+//import static org.firstinspires.ftc.teamcode.control.auto_execution.AutoActions.EXTEND;
+//import static org.firstinspires.ftc.teamcode.control.auto_execution.AutoActions.LIFT;
+//import static org.firstinspires.ftc.teamcode.control.cv.Camera.SPIKE_ZONE;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.bot.Robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoActionHandler {
+    private ArrayList<AutoActions> actionList;
+    private List<AutoActions> currentActions;
+
+    private Robot robot;
+    private Telemetry telemetry;
+
+    private ElapsedTime timer;
+
+    private int totalActions;
+
+
+    public AutoActionHandler(Robot robot, Telemetry telemetry){
+        this.robot = robot;
+        this.telemetry = telemetry;
+
+        actionList = new ArrayList<AutoActions>();
+        currentActions = new ArrayList<AutoActions>();
+        timer = new ElapsedTime();
+    }
+
+    /**
+     * starts the queue
+     */
+    public void init(){
+        if (actionList.isEmpty())
+            return;
+
+        actionList.add(new AutoActions(AutoActions.DONE, false, robot));
+        totalActions = actionList.size();
+
+        addNextActionsToCurrent();
+    }
+
+    /**
+     * runs the action and calls next action in case the current action is complete.
+     */
+    public void run(){
+        for (AutoActions action : currentActions) action.runAction();
+
+        printStatus();
+
+        tryNextAction();
+    }
+
+    /**
+     * @param actionSet a pre-existing set of autoActions to add to this list
+     */
+    public void add(ArrayList<AutoActions> actionSet){
+        actionList.addAll(actionSet);
+    }
+
+    /**
+     * @param actionHandler gets a pre-existing set of actions to add to this list from a pre-built
+     *                      AutoActionHandler
+     */
+    public void add(AutoActionHandler actionHandler){
+        actionList.addAll(actionHandler.getActions());
+    }
+
+    public void add (int action, boolean async, double x, double y, double heading){
+        actionList.add(new AutoActions(action, async, robot, x, y, heading));
+    }
+
+    public void add (int action, boolean async, double[] pos){
+        actionList.add(new AutoActions(action, async, robot, pos[0], pos[1], pos[2]));
+    }
+
+    public void add(int action, boolean async, double value) {
+        actionList.add(new AutoActions(action, async, robot, value));
+    }
+
+    public void add(int action, boolean async, int value) {
+        actionList.add(new AutoActions(action, async, robot, value));
+    }
+
+    public void add(int action, boolean async, int value, int value2) {
+        actionList.add(new AutoActions(action, async, robot, value, value2));
+    }
+
+
+    /**
+     * @param action the identity of the action (see the public static constant in AutoActions)
+     *               This one is for actions that do not require parameters
+     */
+    public void add(int action, boolean async){
+        actionList.add(new AutoActions(action, async, robot));
+    }
+
+
+
+    /**
+     * @return The action list of this object.
+     *
+     * Made for getting presets and adding them to the main AutoRed queue
+     */
+    public ArrayList<AutoActions> getActions(){
+        return actionList;
+    }
+
+    /**
+     * @return the total number of actions queue to execute
+     */
+    public int getTotalActions(){
+        return actionList.size();
+    }
+
+
+
+    /**
+     * Checks the status of the current action and removes the action from queue if isFinished
+     * returns true.
+     */
+    private void tryNextAction(){
+        for (int i = currentActions.size() - 1; i >= 0; i--) {
+            if (currentActions.get(i).isFinished()) currentActions.remove(i);
+        }
+
+        if (currentActions.isEmpty()) {
+            addNextActionsToCurrent();
+
+//            telemetry.addLine();
+//            telemetry.addLine("Moving to next actions:");
+//            for (AutoActions action : currentActions) telemetry.addLine(action.getDescription());
+//            telemetry.update();
+        }
+    }
+
+    public void addNextActionsToCurrent() {
+        boolean isAsync = true;
+        while (isAsync) {
+            AutoActions action = actionList.get(0);
+            currentActions.add(action);
+
+            actionList.remove(0);
+
+            isAsync = action.getAsync();
+        }
+    }
+
+
+
+    /**
+     * Prints the current step in the Auto and gives an idea of how complete the auto is.
+     */
+    public void printStatus(){
+        telemetry.addLine();
+        telemetry.addLine("AutoActionHandler Status:");
+
+        int currentStep = totalActions - actionList.size() + 1;
+
+        if (currentActions.get(0).getIdentity() != AutoActions.DONE) {
+            telemetry.addLine(currentStep + " of " + totalActions + " actions");
+            telemetry.addLine("Current actions description:");
+            for (AutoActions action : currentActions) telemetry.addLine(action.getDescription());
+
+//            telemetry.addLine();
+//            telemetry.addLine("Actions Queued:");
+//            for (AutoActions action : actionList) telemetry.addLine(action.getDescription());
+        }
+        else telemetry.addLine( "Done!");
+    }
+
+//    public void troubleshooting(){
+//        telemetry.addLine();
+//        telemetry.addLine("Troubleshooting AutoActionHandler:");
+//        //put troubleshooting telemetry here.
+//        telemetry.addLine(String.valueOf(current.isFinished()));
+//
+//        telemetry.addLine();
+//    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoActions.java
@@ -0,0 +1,250 @@
+package org.firstinspires.ftc.teamcode.auto;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.PIDController;
+//import org.firstinspires.ftc.teamcode.util.MathHelper;
+//git import org.firstinspires.ftc.teamcode.util.TeleSingle;
+
+
+/*import static org.firstinspires.ftc.teamcode.control.cv.Camera.SPIKE_ZONE;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.centerBackDropPos;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.centerSpikePos;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.leftBackDropPos;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.leftSpikePos;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.rightBackDropPos;
+import static org.firstinspires.ftc.teamcode.control.presets.AutoPresets.rightSpikePos;*/
+
+
+
+    public class AutoActions {
+
+        private Robot robot;
+
+        // identities
+        public static final int DONE = -1;
+        public static final int MOVE = 0;
+        public static final int WAIT = 1;
+//        public static final int GRABBER = 2;
+//        public static final int ARM = 3;
+
+        private int identity;
+        private boolean async;
+        private boolean currentAction = false;
+        private boolean endAction;
+
+        private ElapsedTime timer;
+
+        private String description;
+
+        private double x;
+        private double y;
+        private double heading; // in degrees
+
+        private double waitTime;
+
+        private int value;
+        private int value2;
+
+        private PIDController xPID;
+        private PIDController yPID;
+        private PIDController rotPID;
+
+        private double[] pidOutput = new double[] {0,0,0};
+
+
+        public AutoActions(int id, boolean async, Robot robot){
+            this.async = async;
+            init(id, robot);
+        }
+
+        //Used for id's: MOVE
+        public AutoActions(int id, boolean async, Robot robot, double x, double y, double heading){
+            this.async = async;
+            this.x = x;
+            this.y = y;
+            this.heading = heading;
+
+            xPID = new PIDController(this.x, false);
+            yPID = new PIDController(this.y, false);
+            rotPID = new PIDController(this.heading, true);
+
+            init(id, robot);
+        }
+
+        //Used for id's: WAIT
+        public AutoActions(int id, boolean async, Robot robot, double value){
+            this.async = async;
+            if (id == WAIT) waitTime = value;
+            init(id, robot);
+        }
+
+        //Used for id's: ARM
+//        public AutoActions(int id, boolean async, Robot robot, int value){
+//            this.async = async;
+//            if (id == ARM) this.value = value;
+//            init(id, robot);
+//        }
+
+        //Used for id's: GRABBER
+//        public AutoActions(int id, boolean async, Robot robot, int value, int  value2){
+//            this.async = async;
+//            if (id == GRABBER) {
+//                this.value = value;
+//                this.value2 = value2;
+//            }
+//            init(id, robot);
+//        }
+
+        private void init(int id, Robot robot) {
+            this.identity = id;
+            this.robot = robot;
+            timer = new ElapsedTime();
+        }
+
+        /**
+         * Driving the rob
+         */
+        private void moveTo(){
+            initAction();
+
+            double[] currentPos = robot.odo.getPosition();
+
+            double PIDRampLimit = timer.seconds() * 3;
+
+            TeleSingle.tele.clear();
+            //TeleSingle.tele.addLine("X---------------|");
+            double outputX = MathHelper.clamp(xPID.getPIDOutput(currentPos[0]), -PIDRampLimit, PIDRampLimit);
+            //TeleSingle.tele.addLine("Y---------------|");
+            double outputY = MathHelper.clamp(yPID.getPIDOutput(currentPos[1]), -PIDRampLimit, PIDRampLimit);
+            //TeleSingle.tele.addLine("ROT-------------|");
+            double outputRot = MathHelper.clamp(rotPID.getPIDOutput(currentPos[2]), -PIDRampLimit, PIDRampLimit);
+            TeleSingle.tele.update();
+
+            pidOutput = new double[] {outputX, -outputY, outputRot};
+
+            boolean hasArrived = xPID.hasArrived() && yPID.hasArrived() && rotPID.hasArrived();
+
+            if (!hasArrived) robot.drive.globalDrive(outputX, -outputY, outputRot);
+            else robot.drive.calculateDrivePowers(0, 0, 0);
+
+            boolean timeOut = timer.milliseconds() > 10000;
+            endAction = hasArrived || timeOut;
+        }
+
+
+        /**
+         * waits out timer until timer is greater than or equal to the parameter wait time
+         */
+        private void waiting() {
+            initAction();
+
+            robot.drive.calculateDrivePowers(0,0,0);
+
+            endAction = timer.milliseconds() > (waitTime * 1000);
+        }
+
+//        private void armPos() {
+//            if (!currentAction) robot.arm.setAutoMove(value);
+//
+//            initAction();
+//
+//            endAction = robot.arm.autoMove();
+//        }
+//        //Sets grabber and grabberRot to specified state and ends when both reach their target position
+//
+//        private void grabberState() {
+//            initAction();
+//
+//            robot.grabber.setGrabberState(value == 1, value2);
+//
+//            endAction = true;
+//        }
+
+        private void setEndAutoState(){
+            robot.shutOff();
+        }
+
+        /**
+         * @return whether or not this action has been completed
+         */
+        public boolean isFinished(){
+            return endAction;
+        }
+
+        /**
+         * determines the action and what this specific action will do.
+         */
+        public void runAction(){
+            switch (identity){
+                case DONE:
+                    setEndAutoState();
+                    break;
+                case MOVE:
+                    moveTo();
+                    break;
+                case WAIT:
+                    waiting();
+                    break;
+//                case GRABBER:
+//                    grabberState();
+//                    break;
+//                case ARM:
+//                    armPos();
+//                    break;
+            }
+        }
+
+        /**
+         * helper method to get telemetry text
+         */
+        public String getDescription() {
+            String description = "id: " + identity + "; ";
+            switch (identity){
+                case DONE:
+                    description += "Done!";
+                    break;
+                case MOVE:
+                    description += "Moving to target pos: {"+x+", "+y+", "+heading+"}";
+                    if (currentAction) description += "; PID output: {"
+                            + MathHelper.round100(pidOutput[0]) + ", "
+                            + MathHelper.round100(pidOutput[1]) + ", "
+                            + MathHelper.round100(pidOutput[2]) + "}";
+                    break;
+                case WAIT:
+                    description += "Waiting for "
+                            + (currentAction ? MathHelper.round100(timer.milliseconds() / 1000) : 0)
+                            +  " / " + waitTime + " seconds.";
+                    break;
+                case GRABBER:
+                    description += "Setting Grabber Open: " + (value == 1) + " | Rot State: " + value2;
+                    break;
+                case ARM:
+                    description += "Moving Arm to PosID: " + value;
+                    break;
+            }
+
+            return description;
+        }
+
+        public int getIdentity() { return identity; }
+        public boolean getAsync() { return async; }
+
+        private void initAction(){
+            if (!currentAction){
+                timer.reset();
+                currentAction = true;
+            }
+        }
+
+//    private void checkXSign(){
+//        if (!robot.RED)
+//            this.x *= -1;
+//
+//    }
+//
+//    public int getTimer() {
+//        return (int) (timer.milliseconds() / 1000);
+//    }
+    }
+
